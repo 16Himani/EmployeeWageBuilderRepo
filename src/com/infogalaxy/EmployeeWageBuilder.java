27c99b8a88227b7ae51a80e86b9d72e1b427c592
16Himani <himanipatil016@gmail.com>
2022-03-07 12:51:07 +0530

    MODIFIED: Employee Wage Type calculation

--- src/com/infogalaxy/EmployeeWageBuilder.java
+++ src/com/infogalaxy/EmployeeWageBuilder.java
@@ -5,24 +5,35 @@ import java.util.Random;
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         // UC-2 Employee Wage Calculation
-        final int WAGE_PER_HR= 20;
-        final int FULL_DAY_HR= 8;
-        int total_wage =0;
+        //UC-2 Employee Work Type
+        final int WAGE_PER_HR = 20;
+        final int FULL_TIME_HR = 8;
+        final int PART_TIME_HR = 4;
+        int total_wage = 0;
         //UC-1 Employee Attendance
 
-    Random r = new Random();
-    int empAttendance = r.nextInt()%2;
-    if(empAttendance==0) {
-        System.out.println("Employee is Apsent");
+        Random random = new Random();
+        int empAttendance = random.nextInt() % 2;
+        if (empAttendance == 0) {
+            System.out.println("Employee is Apsent");
+
+        } else {
+            System.out.println("Employee is Present");
+            int empType = random.nextInt() % 2;
+            if (empType == 0) {
+                System.out.println("Employee is Part-Time");
+                total_wage = (WAGE_PER_HR * PART_TIME_HR);
+            } else {
+                System.out.println("Employee is Full-Time");
+                total_wage = (WAGE_PER_HR * FULL_TIME_HR);
+
+            }
+
+
+            System.out.println("Total Wage=" + total_wage);
+
+        }
+
 
     }
-    else{
-        System.out.println("Employee is Present");
-        total_wage= WAGE_PER_HR * FULL_DAY_HR;
-    }
-        System.out.println("Total Wage="+total_wage);
-
-    }
-
-
 }
