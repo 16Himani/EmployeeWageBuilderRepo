2f285df13f38ee903ae63ed6a2ad3f4eed67a123
16Himani <himanipatil016@gmail.com>
2022-03-07 13:17:31 +0530

    MODIFIED: Switch Case

--- src/com/infogalaxy/EmployeeWageBuilder.java
+++ src/com/infogalaxy/EmployeeWageBuilder.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         // UC-2 Employee Wage Calculation
-        //UC-2 Employee Work Type
+        //UC-3 Employee Work Type
         final int WAGE_PER_HR = 20;
         final int FULL_TIME_HR = 8;
         final int PART_TIME_HR = 4;
@@ -15,21 +15,23 @@ public class EmployeeWageBuilder {
         Random random = new Random();
         int empAttendance = random.nextInt() % 2;
         if (empAttendance == 0) {
-            System.out.println("Employee is Apsent");
+            System.out.println("Employee is Absent");
 
         } else {
             System.out.println("Employee is Present");
             int empType = random.nextInt() % 2;
-            if (empType == 0) {
+             switch (empType ) {
+                 case 0:
                 System.out.println("Employee is Part-Time");
                 total_wage = (WAGE_PER_HR * PART_TIME_HR);
-            } else {
+                break;
+                 case 1:
                 System.out.println("Employee is Full-Time");
                 total_wage = (WAGE_PER_HR * FULL_TIME_HR);
+                break;
 
             }
-
-
+            //UC-4 SWITCH CASE
             System.out.println("Total Wage=" + total_wage);
 
         }
