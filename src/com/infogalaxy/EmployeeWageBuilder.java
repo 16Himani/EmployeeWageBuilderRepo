397deee27a86bd921f7e901a08a903ca89751570
16Himani <himanipatil016@gmail.com>
2022-03-08 12:49:53 +0530

    MODIFIED: Employee Wage for Month

--- src/com/infogalaxy/EmployeeWageBuilder.java
+++ src/com/infogalaxy/EmployeeWageBuilder.java
@@ -1,41 +1,50 @@
 package com.infogalaxy;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         // UC-2 Employee Wage Calculation
         //UC-3 Employee Work Type
+        //UC-5 Calculating Wages for a Month
         final int WAGE_PER_HR = 20;
         final int FULL_TIME_HR = 8;
         final int PART_TIME_HR = 4;
+        final int WORKING_DAYS = 20;
         int total_wage = 0;
         //UC-1 Employee Attendance
 
         Random random = new Random();
-        int empAttendance = random.nextInt() % 2;
-        if (empAttendance == 0) {
-            System.out.println("Employee is Absent");
+        for (int i = 0; i <= 20; i++) {
+            System.out.println("Day " + i);
+            int empAttendance = random.nextInt() % 2;
+            if (empAttendance == 0) {
+                System.out.println("Employee is Absent");
 
-        } else {
-            System.out.println("Employee is Present");
-            int empType = random.nextInt() % 2;
-             switch (empType ) {
-                 case 0:
-                System.out.println("Employee is Part-Time");
-                total_wage = (WAGE_PER_HR * PART_TIME_HR);
-                break;
-                 case 1:
-                System.out.println("Employee is Full-Time");
-                total_wage = (WAGE_PER_HR * FULL_TIME_HR);
-                break;
+            } else {
+                System.out.println("Employee is Present");
+                int empType = random.nextInt() % 2;
+                //UC-4 Convert Switch Case
+                switch (empType) {
+                    case 0:
+                        System.out.println("Employee is Part-Time");
+                        total_wage = (WAGE_PER_HR * PART_TIME_HR);
+                        break;
+                    case 1:
+                        System.out.println("Employee is Full-Time");
+                        total_wage = (WAGE_PER_HR * FULL_TIME_HR);
+                        break;
+
+                }
 
             }
-            //UC-4 SWITCH CASE
-            System.out.println("Total Wage=" + total_wage);
 
         }
 
+
+        total_wage = total_wage * WORKING_DAYS;
+        System.out.println("Total Salary" + total_wage);
 
     }
 }
